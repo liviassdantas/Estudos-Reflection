abfd923fd5dec8bad7f9ebb0c8368408fc396bd0
Livia Dantas <livia.ss.dantas@gmail.com>
2020-10-15 17:53:54 -0300

    Initial commit

---
+++ Reflection/Infraestrutura/WebApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection.Infraestrutura
+{
+    public class WebApplication
+    {
+        public void Iniciar()
+        {
+
+        }
+    }
+}
